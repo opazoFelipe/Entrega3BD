d73fd9699bc7a1e3397fe7648a21b1922d2e679d
felipe opazo <felipe.orivas@gmail.com>
2019-07-13 14:57:17 -0400

    Avanzando en la interfaz rama alternativa

---
+++ controladores/consultasE2/showQueries.js
@@ -0,0 +1,4 @@
+function alerta()
+{
+    alert('consulta1');
+}
---
+++ vistas/home.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bodyStyle.css">
+    <link rel="stylesheet" href="css/navStyle.css">
+    <script src="../controladores/consultasE2/showQueries.js"></script>
+    <title>Aplicacion BD</title>
+</head>
+<body>
+    <div id="header">
+        <p>Aplicacion Entrega 3 Base de Datos</p>
+        <p style="margin:-1.5% 0;">Felipe Opazo Rivas</p>
+    </div>
+    <div id="nav">
+        <ul>
+            <li>Asignaturas
+                <ul>
+                    <li>Agregar Asignatura</a></li>
+                    <li>Modificar Asignatura</a></li>
+                    <li>Eliminar Asignatura</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul>
+            <li>Consultas E2
+                <ul>
+                    <li onclick="alerta()">Ver Consulta 1</li>
+                    <li onclick="alerta()">Ver Consulta 2</li>
+                    <li onclick="alerta()">Ver Consulta 3</li>
+                    <li onclick="alerta()">Ver Consulta 4</li>
+                    <li onclick="alerta()">Ver Consulta 5</li>
+                    <li onclick="alerta()">Ver Consulta 6</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div id="body">
+        <table>
+            <tr>
+                <th>columna</th>
+                <th>columna</th>
+                <th>columna</th>
+                <th>columna</th>
+                <th>columna</th>
+            </tr>
+        </table>
+    </div>
+    <div id="footer"></div>
+</body>
+</html>
