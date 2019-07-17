563e6acd11f71a312d8c874215a20a15994d5efd
felipe <felipe.orivas@gmail.com>
2019-07-17 00:45:02 -0400

    ordeno esta rama

--- vistas/home.php
+++ vistas/home.php
@@ -4,12 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bodyStyle.css">
-    <link rel="stylesheet" href="css/navStyle.css">
-    <link rel="stylesheet" href="css/tablas.css">
     <script src="../controladores/consultasE2/showQueries.js"></script>
     <script src="../controladores/asignatura/showAsig.js"></script>
-
     <title>Aplicacion BD</title>
 </head>
 <body>
